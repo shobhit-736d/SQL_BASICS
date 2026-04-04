5e4cd740273583fb179c037204cd205fbc6a9bf3
shobhit-736d <shobhit736d@gmail.com>
2026-04-04 14:17:40 +0530

    DML

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,3 +1,8 @@
-select * 
-from orders
-where sales > 15
+-- Insert new customers in a table
+
+INSERT INTO customers (id, first_name, country, score )
+VALUES 
+	(6, 'Arpit', 'India', 970),
+	(7, 'Robin', 'Russia', NULL)
+
+SELECT * FROM customers
